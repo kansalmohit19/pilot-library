fcc606805a5ff48c3f9915fabca71a1199aa0c1e
Mohit Kansal <kansalmohit19@gmail.com>
2025-09-02 16:51:07 +0530

    Shell script updated

--- buildDocs.sh
+++ buildDocs.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SEDOPTION="-i ''"   # macOS
+else
+  SEDOPTION="-i"      # Linux
+fi
+
 DOCS_ROOT=docs
 
 if [ -d "$DOCS_ROOT" ]; then
@@ -11,6 +17,9 @@ fi
 mkdir -p "$DOCS_ROOT"
 
 cp README.md $DOCS_ROOT/index.md
+cp LICENSE.md $DOCS_ROOT/license.md
 cp samplelibrary/README.md $DOCS_ROOT/sample-library.md
 cp samplelibrary/VIEWMODEL.md $DOCS_ROOT/view-model.md
+
+eval sed $SEDOPTION -e 's/LICENSE.md/license/g' "$DOCS_ROOT/index.md"
 
