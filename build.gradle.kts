a8ec18404f69817f1dac8e43787736b4e5529f4d
Mohit Kansal <kansalmohit19@gmail.com>
2025-12-01 20:29:10 +0530

    [WIP]: TODO Removed versions catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.library) apply false
+    /*alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.hilt.android).apply(false)
-    alias(libs.plugins.ksp).apply(false)
+    alias(libs.plugins.ksp).apply(false)*/
+
+    id("com.android.library") version("8.8.1") apply false
+    id("org.jetbrains.kotlin.android") version("2.0.21") apply false
+    id("org.jetbrains.kotlin.jvm") version("2.0.21") apply false
+    id("com.google.dagger.hilt.android") version("2.52") apply false
+    id("com.google.devtools.ksp") version("2.0.21-1.0.27") apply false
 }
--- locationlibrary/build.gradle.kts
+++ locationlibrary/build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
-    alias(libs.plugins.android.library)
+    /*alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     kotlin("kapt")
     alias(libs.plugins.ksp)
-    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.hilt.android)*/
+
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+
 }
 android {
     namespace = "com.mohit.locationlibrary"
@@ -35,13 +42,19 @@ android {
 }
 
 dependencies {
-    implementation(libs.hilt.android)
+    /*implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
-
     implementation(libs.google.location)
     implementation(libs.lifecycle.service)
-    implementation(libs.lifecycle.runtime)
+    implementation(libs.lifecycle.runtime)*/
 
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-compiler:2.52")
+    implementation("androidx.room:room-runtime:2.8.4")
+    ksp("androidx.room:room-compiler:2.8.4")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("androidx.lifecycle:lifecycle-service:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
 }
